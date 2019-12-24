e98fc5b69a01676d4ac3c2dd7094105d1063f8a6
Fan Pu <zengfanpu@gmail.com>
2019-12-24 04:22:26 -0500

    Change to twilight color scheme

--- config.h
+++ config.h
@@ -88,43 +88,32 @@ float alphaUnfocussed = 0.6; //< alpha value used when the focus is lost
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
+  "#1e1e1e", /* base00 */
+  "#cf6a4c", /* base08 */
+  "#8f9d6a", /* base0B */
+  "#f9ee98", /* base0A */
+  "#7587a6", /* base0D */
+  "#9b859d", /* base0E */
+  "#afc4db", /* base0C */
+  "#a7a7a7", /* base05 */
+  "#5f5a60", /* base03 */
+  "#cda869", /* base09 */
+  "#323537", /* base01 */
+  "#464b50", /* base02 */
+  "#838184", /* base04 */
+  "#c3c3c3", /* base06 */
+  "#9b703f", /* base0F */
+  "#ffffff", /* base07 */
 };
-
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 13;
+static unsigned int defaultrcs = 0;
 
 /*
  * Default shape of cursor
